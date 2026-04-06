792e43b4e9e5750e38cb31e440de940073000c0c
Matt Albinson <mattalbinson@berkeley.net>
2026-04-06 03:47:45 +0000

    object

--- script.js
+++ script.js
@@ -1,29 +1,39 @@
-let x = 100;
-let y = 100;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
     background(200);
-    fill("green")
+    fill("green");
+    stroke("yellow");
+    b1 = new Ball();
 }
 
 function draw() {
     background(200);
-    circle(x, y, 25)
+    b1.update();
 }
 
 function keyPressed() {
     if (key === 'i') {
-        y -= 10;
+        b1.y -= 10;
     }
     if (key === 'j') {
-        x -= 10;
+        b1.x -= 10;
     }
     if (key === 'k') {
-        y += 10;
+        b1.y += 10;
     }
     if (key === 'l') {
-        x += 10;
+        b1.x += 10;
+    }
+}
+
+class Ball {
+    constructor(){
+        this.x = 100;
+        this.y = 100;
+    }
+    update(){
+        circle(this.x, this.y, 25);
     }
 }
 
